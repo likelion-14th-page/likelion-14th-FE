c516aeda80ffbf34857386bc41c76302d7361c1d
t-timda <jung020623@g.hongik.ac.kr>
2026-01-21 19:33:45 +0900

    #2 [UI] 네브바 상단 고정

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,20 +5,17 @@ import PageHeader from '../components/common/PageHeader';
 const Layout = () => {
   const location = useLocation();
 
-  // 페이지별 헤더 내용 (URL 경로 : 내용)
   const headerConfig = {
     '/events': {
       subtitle: '행사 일정',
       title: 'Program',
       description: '2024년 멋쟁이사자처럼 14기에서 진행할\n1년 로드맵을 소개합니다',
     },
-
     '/archaive': {
       subtitle: '지난 활동',
       title: 'Archaive',
       description: '2025년 멋쟁이 사자처럼 13기에서 진행한\n프로젝트와 활동을 소개합니다.',
     },
-
     '/recruiting': {
       subtitle: '모집 안내',
       title: 'Recruiting',
@@ -26,24 +23,33 @@ const Layout = () => {
     },
   };
 
-  // 현재 주소에 맞는 정보 가져오기
   const currentHeader = headerConfig[location.pathname];
 
   return (
     <div className="w-full min-h-screen bg-bg-dark">
-      <Navbar />
-      {currentHeader && (
-        <PageHeader 
-          subtitle={currentHeader.subtitle}
-          title={currentHeader.title}
-          description={currentHeader.description}
-        />
-      )}
       
-      {/* 본문 영역: 헤더와 라인을 맞추기 위해 px-[120px] 적용 */}
-      <main className="w-full px-[120px] py-10">
-        <Outlet />
-      </main>
+      {/* 1. 네브바 고정 영역 */}
+      <div className="fixed top-0 left-0 w-full z-50 bg-bg-dark"> 
+        <Navbar />
+      </div>
+
+      {/* 2. 컨텐츠 영역 (네브바 높이만큼 아래로 밀기) */}
+      <div className="pt-[100px] w-full">
+        
+        {currentHeader && (
+          <PageHeader 
+            subtitle={currentHeader.subtitle}
+            title={currentHeader.title}
+            description={currentHeader.description}
+          />
+        )}
+        
+        {/* 본문 영역 */}
+        <main className="w-full px-[120px] py-10">
+          <Outlet />
+        </main>
+        
+      </div>
     </div>
   );
 };
