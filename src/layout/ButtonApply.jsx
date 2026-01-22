7e40b2ce0541047e7314743aa1b815376abbfec9
이윤서 <yyoo04@naver.com>
2026-01-22 19:26:48 +0900

    #3 [UI] 버튼 컴포넌트 트랜지션 추가

--- src/layout/ButtonApply.jsx
+++ src/layout/ButtonApply.jsx
@@ -1,34 +1,36 @@
 import arrow from '../assets/icon/arrow.svg';
 
 const ButtonApply = ({ type, className, onClick }) => {
-  const long =
-    ' pl-[32px] pr-[24px] py-[8px] text-[24px] font-gray-01 whitespace-nowrap';
-  const short = 'w-[95px] px-[16px] py-[8px] text-[18px] whitespace-nowrap';
-
-  const long_text = '홍익대학교 멋사 14기 지원하기';
-  const short_text = '지원하기';
+  const long_title =
+    ' w-fit pl-[32px] pr-[24px] py-[16px] title-24-semibold text-gray-01 border-orange-04';
+  const short = 'w-fit px-[16px] py-[8px] body-16-semibold border-orange-02';
+  const long_footer =
+    'pl-[32px] pr-[24px] py-[16px] title-20-bold text-gray-01 border-orange-04';
 
   return (
     <button
       onClick={onClick}
-      className={
-        'button_layout button_text flex justify-center items-center ' +
-        (type === 'long' ? long : short) +
-        ' ' +
-        (className || '')
-      }
+      className={`
+        flex justify-center items-center whitespace-nowrap rounded-full border-[1.5px] 
+        bg-orange-01 hover:bg-orange-01-hover transition-colors duration-300 cursor-pointer
+        ${type === 'long_title' && long_title}
+        ${type === 'short' && short}
+        ${type === 'long_footer' && long_footer}
+        ${className || ''}
+      `}
     >
-      {type === 'long' && (
+      {type === 'long_title' || type === 'long_footer' ? (
         <div className="flex justify-center items-center">
-          <span>{long_text}</span>
+          <span>홍익대학교 멋사 14기 지원하기</span>
           <img
             src={arrow}
             alt="arrow"
             className="ml-[10px] w-[31px] flex-shrink-0"
           />
         </div>
+      ) : (
+        '지원하기'
       )}
-      {type === 'short' && short_text}
     </button>
   );
 };
