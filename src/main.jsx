e52744f1a82ba63d158d2471cf57ef421f21dba3
t-timda <jung020623@g.hongik.ac.kr>
2026-01-22 21:26:18 +0900

    #2 [UI] 추후공지 글자색 수정

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import 'pretendard/dist/web/variable/pretendardvariable.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
